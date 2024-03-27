e3777540be3bcfe9c8d7eccae41a8dd5ea7db52e
shuaib128 <shueaus@gmail.com>
2024-03-27 13:18:31 -0700

    added CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -16,12 +16,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    'https://web-production-3d4a.up.railway.app',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-3d4a.up.railway.app',
 ]
 
 USERNAME_FIELD = 'email'
